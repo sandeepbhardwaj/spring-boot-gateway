167a9a3fb4c25c5e0b0e5ae3e51c7304e098e6f2
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2022-01-16 18:11:23 +0530

    Initial commit

---
+++ src/main/java/com/example/gateway/SpringBootGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGatewayApplication.class, args);
+	}
+
+}
